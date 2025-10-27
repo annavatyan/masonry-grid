cd5141ba0cbac364b9ccca2ee25d395776b8b75a
Anna <vatyan.anna@gmail.com>
2025-10-27 17:16:19 +0400

    Add Infinite scroll to Masonry grid, change css columns arrangment to distributed columns calculation

--- src/components/MasonryGrid/MasonryGrid.tsx
+++ src/components/MasonryGrid/MasonryGrid.tsx
@@ -1,24 +1,55 @@
+import { useEffect, useMemo, useRef } from 'react';
 import useFetchPhotos from '../../hooks/useFetchPhotos';
 import PhotoCard from './PhotoCard';
+import type { Photo } from "../../utils/types";
 
-const MasonryGrid = ({searchTerm, columns}:
-     {searchTerm: string, columns: number}) => {
-  const { photos, loading, error } = useFetchPhotos(searchTerm);
+const MasonryGrid = ({searchTerm, columns}: {searchTerm: string, columns: number}) => {
+  const { photos, loading, error, loadMore } = useFetchPhotos(searchTerm);
+
+  const loadMoreRef = useRef<HTMLDivElement | null>(null);
+
+  // Infinite scroll
+  useEffect(() => {
+    if (!loadMoreRef.current) return;
+  
+    const observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        loadMore(); // fetch next bucket of items
+      }
+    }, { rootMargin: "200px", threshold: 0.1  });
+  
+    observer.observe(loadMoreRef.current);
+    return () => observer.disconnect();
+  }, [loadMore]);
+
+   // Split photos into columns
+    const distributed = useMemo(() => {
+        const cols: Photo[][] = Array.from({ length: columns }, () => []);
+        photos.forEach((photo, idx) => {
+        cols[idx % columns].push(photo);
+        });
+        return cols;
+    }, [photos, columns]);
  
   if (error) return <p className="text-red-500 text-center">{error}</p>;
   if (loading && !photos.length) return <p className="text-center">Loading...</p>;
 
   return (
     <div className="masonry-grid">
-        <div className="masonry-css" style={{columnCount: columns}}>
-            {photos.map(photo => (
-              <div  key={photo.id} className="mb-4">
-                <PhotoCard photo={photo} />
-              </div>  
+        <div className="flex gap-4 justify-center items-start">
+        {distributed.map((col, idx) => (
+            <div key={idx} className="flex flex-col gap-4 w-full">
+            {col.map((photo) => (
+                <PhotoCard key={photo.id} photo={photo} />
             ))}
+            </div>
+        ))}
         </div>
 
         {loading && <p className="text-center mt-4">Loading more...</p>}
+
+        {/* Element for infinite scroll */}
+        <div ref={loadMoreRef} className="h-2" />
     </div>
   );
 };
--- src/hooks/useFetchPhotos.tsx
+++ src/hooks/useFetchPhotos.tsx
@@ -1,23 +1,35 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { fetchPhotos } from "../utils/api";
 import type { Photo } from "../utils/types";
 import { GRID_PER_PAGE } from "../utils/constants";
 
-const useFetchPhotos = (query: string, perPage=GRID_PER_PAGE, page=1) => {
-    const [photos, setPhotos] = useState<Photo[]>([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<string | null>(null);
+const useFetchPhotos = (query: string, perPage = GRID_PER_PAGE) => {
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [page, setPage] = useState(1);
 
-    useEffect(() => {
-        setLoading(true);
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
 
-        fetchPhotos(query, perPage, page)
-        .then(setPhotos)
-        .catch(err => setError(err.message))
-        .finally(() => setLoading(false));
-    }, [query, perPage, page]);
+    fetchPhotos(query, perPage, page)
+      .then(newPhotos => {
+        setPhotos(prev => page === 1 ? newPhotos : [...prev, ...newPhotos]);
+      })
+      .catch(err => setError(err.message))
+      .finally(() => setLoading(false));
+  }, [query, perPage, page]);
 
-    return {photos, loading, error};
+   // Reset on query changes
+   useEffect(() => {
+    setPhotos([]);
+    setPage(1);
+  }, [query]);
+
+  const loadMore = useCallback(() => setPage(prev => prev + 1), []);
+
+  return { photos, loading, error, loadMore };
 };
 
 export default useFetchPhotos;
