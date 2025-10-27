f6306c7f0fc5d1c7d7378c00622c7e7f22e378b2
Anna <vatyan.anna@gmail.com>
2025-10-27 11:58:59 +0400

    Setup Searchbar on Gallery page

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { useDebounce } from "../../hooks/useDebounce";
+
+interface SearchBarProps {
+  value?: string;
+  onChange: (value: string) => void;
+}
+
+export default function SearchBar({ value = "", onChange }: SearchBarProps) {
+  const [inputValue, setInputValue] = useState<string>(value);
+  const debouncedInput = useDebounce(inputValue, 300);
+
+  useEffect(() => {
+    if (debouncedInput !== value) {
+      onChange(debouncedInput);
+    }
+  }, [debouncedInput, value, onChange]);
+
+  return (
+    <div className="relative w-full max-w-2xl m-auto mb-5">
+        <input
+            type="text"
+            placeholder="Search ..."
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            className="w-full pl-10 pr-4 py-2
+                       border border-gray-300
+                       rounded-lg shadow-sm"
+        />
+        <svg
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-4.35-4.35m1.7-5.15a7 7 0 11-14 0 7 7 0 0114 0z" />
+        </svg>
+    </div>
+  );
+}
--- src/pages/GalleryPage/GalleryPage.tsx
+++ src/pages/GalleryPage/GalleryPage.tsx
@@ -1,12 +1,19 @@
 import { Link } from 'react-router-dom';
 import useFetchPhotos from '../../hooks/useFetchPhotos';
+import SearchBar from '../../components/SearchBar';
+import { useState } from 'react';
 
 const GalleryPage = () => {
-  const { photos, loading, error } = useFetchPhotos("Portugal");
+  const [searchTerm, setSearchTerm] = useState<string>("Portugal")
+  const { photos, loading, error } = useFetchPhotos(searchTerm);
 
   return (
     <div className='container mx-auto px-4'>
-        <h1>Gallery Page</h1>
+        <h1 className="text-4xl font-bold text-center text-gray-800 m-5">
+          Masonry Grid
+        </h1>
+
+        <SearchBar value={searchTerm}  onChange={setSearchTerm}/>
 
         {error && <p>Error - {error}</p>}
 
