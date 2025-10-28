d02a4ef950ba3716d951092132e4e60f1aeda8e5
Anna <vatyan.anna@gmail.com>
2025-10-28 15:16:26 +0400

    Add responsive support for grid columns

--- src/components/MasonryGrid/MasonryGrid.tsx
+++ src/components/MasonryGrid/MasonryGrid.tsx
@@ -1,14 +1,32 @@
-import { useEffect, useMemo, useRef } from 'react';
+import { useState, useEffect, useMemo, useRef } from 'react';
 import useFetchPhotos from '../../hooks/useFetchPhotos';
 import PhotoCard from './PhotoCard';
 import type { Photo } from "../../utils/types";
 import Spinner from '../Spinner';
 import StatusMessage from '../StatusMessage/StatusMessage';
 
-const MasonryGrid = ({searchTerm, columns}: {searchTerm: string, columns: number}) => {
+const MasonryGrid = ({searchTerm, columns: defaultColumns}: {searchTerm: string, columns: number}) => {
   const { photos, loading, error, loadMore } = useFetchPhotos(searchTerm);
 
+  const containerRef = useRef<HTMLDivElement | null>(null);
   const loadMoreRef = useRef<HTMLDivElement | null>(null);
+
+  const [columns, setColumns] = useState(defaultColumns);
+
+  //Responsive columns
+  useEffect(() => {
+    const handleResize = () => {
+      const width = containerRef.current?.offsetWidth || window.innerWidth;
+
+      if (width < 600) setColumns(2);
+      else if (width < 900) setColumns(3);
+      else setColumns(defaultColumns);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [defaultColumns]);
 
   // Infinite scroll
   useEffect(() => {
@@ -39,7 +57,7 @@ const MasonryGrid = ({searchTerm, columns}: {searchTerm: string, columns: number
 
 
   return (
-    <section className="masonry-grid" aria-label="Photo Gallery">
+    <section ref={containerRef} className="masonry-grid" aria-label="Photo Gallery">
         <div className="flex gap-4 justify-center items-start">
           {distributed.map((col, idx) => (
               <div key={idx} className="flex flex-col gap-4 flex-1">
